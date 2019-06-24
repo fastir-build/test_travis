45679a315ef77991c3a26a32c7e9974d9e3c1485
Antoine <ant1@users.noreply.github.com>
2019-06-24 16:00:43 +0000

    Check build #5

--- checkbuild.py
+++ checkbuild.py
@@ -5,8 +5,10 @@ import sys
 def Main():
     if sys.platform == 'linux':
         command = os.path.join('dist', 'fastir_artifacts', 'fastir_artifacts')
+        command =  ' '.join(['sudo', command])
     elif sys.platform == 'darwin':
         command = os.path.join('dist', 'fastir_artifacts', 'fastir_artifacts')
+        command =  ' '.join(['sudo', command])
     elif sys.platform == 'win32':
         command = os.path.join('dist', 'fastir_artifacts', 'fastir_artifacts.exe')
     else:
